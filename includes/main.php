eb2b8478f36e3f02c908733bc566e55c7b602c2e
Nouira Alim <nouiraalim@gmail.com>
2022-01-27 15:47:39 +0100

    Inclusion de la page à charger
    Ajout de la page récupérér depuis l'url  dans le main

--- includes/main.php
+++ includes/main.php
@@ -2,4 +2,7 @@
 require_once "./includes/nav.php";
 ?>
 <h1>GameLib</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui neque autem perferendis ex aliquid excepturi quae sed quis soluta nisi.</p>
+<?php
+
+$page = $_GET["page"] ?? "./includes/404.php";
+require_once $page;
