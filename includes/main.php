ec4a8406bb4181e0a56692a2bd8bf6c4d66766b4
Nouira Alim <nouiraalim@gmail.com>
2022-01-27 15:39:16 +0100

    Création et inclusion du fichier nav.php

--- includes/main.php
+++ includes/main.php
@@ -1,2 +1,5 @@
+<?php
+require_once "./includes/nav.php";
+?>
 <h1>GameLib</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui neque autem perferendis ex aliquid excepturi quae sed quis soluta nisi.</p>
