0d5636bb49f4510cdde4086ece6292d7e9f1fe1c
Nouira Alim <nouiraalim@gmail.com>
2022-02-02 09:48:22 +0100

    Modification des formulaires

--- includes/formulaire.inc.php
+++ includes/formulaire.inc.php
@@ -1,14 +1,81 @@
 <?php
 
-include "frmFormulaire.php";
 
-if (isset($_POST["frm"])) 
+if (isset ($_POST['frm'])) 
 {
-    echo "Je viens du formulaire";
-}
-
-else
-{
-    echo "Je viens du futur!";
-}
+    $nom = htmlentities(trim($_POST['nom'])) ?? '';
+    $prenom = htmlentities(trim($_POST['prenom'])) ?? '';
+    $email = htmlentities(trim($_POST['email'])) ?? '';
+    $password = $_POST["password"];
+    $passwordRepeat = $_POST["passwordRepeat"];
     
+    $erreur = array(); //Tableau vide
+
+    if (strlen(trim($nom)) === 0)
+    {
+        array_push($erreur, "Veuillez saisir votre nom");
+    }
+    else if (!ctype_alpha($nom))
+    {
+        array_push($erreur, "Veuillez saisir des caractères alphabétiques pour votre nom");
+    }
+
+    if (strlen(trim($prenom)) === 0)
+    {
+        array_push($erreur, "Veuillez saisir votre prenom");
+    }
+    else if (!ctype_alpha($prenom))
+    {
+        array_push($erreur, "Veuillez saisir des caractères alphabétiques pour votre prénom");
+    }
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        array_push($erreur, "Veuillez saisir un e-mail valide");
+    }
+
+    if (strlen($password) === 0)
+    {
+        array_push($erreur, "Veuillez saisir un mot de passe");
+    }
+
+    if (strlen($passwordRepeat) === 0)
+    {
+        array_push($erreur, "Veuillez saisir la vérificatio  de votre mot de passe");
+    }
+
+    if  ($password !== $passwordRepeat)
+    {
+        array_push($erreur, "Veuillez saisir le même mot de passe");
+    }
+
+    if (count($erreur) === 0)
+    {
+        echo "Traitement du formulaire";
+    }
+
+    else
+    {
+        $messageErreur = "<ul>";
+        $cnt = 0;
+        
+        do 
+        {
+            $messageErreur .= "<li>" . $erreur[$cnt] . "</li>";
+            ++$cnt;
+        } 
+        while ($cnt <count($erreur));
+        
+        $messageErreur .= "</ul>";
+        
+        echo $messageErreur;
+    }
+
+}
+else 
+{
+    echo "Merci de renseigner le formulaire";
+    $nom = $prenom = $email = '';
+}
+
+include 'frmFormulaire.php';
--- includes/frmFormulaire.php
+++ includes/frmFormulaire.php
@@ -1,7 +1,14 @@
 <form method="post" action="index.php?page=formulaire">
-    <label for="nom">Nom :</label><input type="text" id="nom" name="nom"/><br/>
-    <label for="prenom">Prénom :</label><input type="text" id="prenom" name="prenom"/><br/>
-    <label for="email">e-mail :</label><input type="text" id="email" name="email"/><br/>
+<label for="nom"> Nom : </label>
+        <input type="texte" id="nom" name="nom" value="<?php echo $nom;?>" /><br />
+    <label for="prenom"> Prénom : </label>
+        <input type="texte" id="prenom" name="prenom" value="<?php echo $prenom;?>" /><br />
+    <label for="email"> E-mail : </label>
+        <input type="texte" id="email" name="email" value="<?php echo $email;?>" /><br />
+    <label for="password"> Mot de passe : </label>
+        <input type="password" id="password" name="password" value="" /><br />
+    <label for="passwordRepeat"> Confirmer votre mot de passe : </label>
+        <input type="password" id="passwordRepeat" name="passwordRepeat" value="" /><br />
     <input type="reset" value="Effacer" />
     <input type="submit" value="Clique-moi grand fou!"/>
     <input type="hidden" name="frm" />
